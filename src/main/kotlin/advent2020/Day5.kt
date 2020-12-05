0517762c0daa69800d50114b4f45266d4b298fd6
Luke Durkan <ldurkan@atlassian.com>
2020-12-05 22:11:48 +1100

    Refined solution, moved to group by for part 2 and adding an extension function to improve readability. Migrated magic numbers to constansts.

--- src/main/kotlin/advent2020/Day5.kt
+++ src/main/kotlin/advent2020/Day5.kt
@@ -4,27 +4,22 @@ import kotlin.math.abs
 import kotlin.math.ceil
 import kotlin.math.floor
 
-fun main() {
-    println(Day5(Resources.resourceAsList("day5.input")).solvePart2())
-}
-
 class Day5(private val passes : List<String>) {
 
     fun solvePart1() : Int = passes.map { getSeatId(it)}
         .maxOrNull() ?: error("Couldn't find max seat ID")
 
     fun solvePart2(): Int =
-        passes.map { getSeatId(it) }
+        (passes.map { getSeatId(it) }
             .sorted()
             .zipWithNext()
-            .map { it to abs(it.first - it.second) }
-            .first { it.second == 2 }.first
-            .first + 1
-
+            .groupBy { abs(it.first - it.second) }[2] ?: error("Couldn't find seat ID gap"))
+            .first()
+            .getIdInMiddle()
 
     private fun getSeatId(pass : String) : Int {
-        val row =  findPosition(pass.take(7), 127)
-        val column = findPosition(pass.takeLast(3), 7)
+        val row =  findPosition(pass.take(ROW_PARTITION_SIZE), MAX_ROW_NUM)
+        val column = findPosition(pass.takeLast(COL_PARTITION_SIZE), MAX_COL_NUM)
 
         return row * 8 + column
     }
@@ -33,16 +28,27 @@ class Day5(private val passes : List<String>) {
         var currentMax = max.toDouble()
         var currentMin = min.toDouble()
 
-        for (index in 0 until input.length - 1) {
-            if (input[index] == LOWER_ROW || input[index] == LOWER_COL) currentMax = floor((currentMin + currentMax) / 2)
-            else currentMin = ceil((currentMin + currentMax) / 2)
+        for (pos in 0 until input.length - 1) {
+            if (input[pos] == LOWER_ROW || input[pos] == LOWER_COL)
+                currentMax = floor((currentMin + currentMax) / 2)
+            else
+                currentMin = ceil((currentMin + currentMax) / 2)
         }
 
-        return if (input.last() == LOWER_COL || input.last() == LOWER_ROW) currentMin.toInt() else currentMax.toInt()
+        return if (input.last() == LOWER_COL || input.last() == LOWER_ROW)
+            currentMin.toInt()
+        else
+            currentMax.toInt()
     }
 
+    private fun Pair<Int, Int>.getIdInMiddle()  : Int = this.first + 1
+
     companion object {
-        const val LOWER_ROW = 'F'
-        const val LOWER_COL = 'L'
+        private const val LOWER_ROW = 'F'
+        private const val LOWER_COL = 'L'
+        private const val ROW_PARTITION_SIZE = 7
+        private const val COL_PARTITION_SIZE = 3
+        private const val MAX_ROW_NUM = 127
+        private const val MAX_COL_NUM = 7
     }
 }
