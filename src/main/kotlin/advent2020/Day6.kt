1ef9c9639bad78f2df52ef54532562670decf9bb
Luke Durkan <ldurkan@atlassian.com>
2020-12-06 16:42:52 +1100

    Refined day 6 solution, extracted code in solve methods to own functions for easier readability.

--- src/main/kotlin/advent2020/Day6.kt
+++ src/main/kotlin/advent2020/Day6.kt
@@ -1,28 +1,24 @@
 package advent2020
 
-fun main() {
-    println(Day6(Resources.resourceAsString("day6.input")).solvePart2())
-}
-
 class Day6(input : String) {
     private val answers = parse(input)
 
-    fun solvePart1() : Int = answers.map {it.replace("\n", "")}
-        .map { answer ->
-        answer.groupBy { it }
-            .keys
-            .size
-    }.sum()
+    fun solvePart1() : Int = answers.map { numUniqueQuestionsAnsweredYes(it) }.sum()
 
-    fun solvePart2() : Int =
-        answers.map { answer ->
-            val answerMap = answer.groupingBy { it }
-                .eachCount()
-            val numPeople = answerMap.getOrDefault('\n', 0) + 1
-            answerMap.filterValues { it == numPeople }
-                .count()
-        }.sum()
+    fun solvePart2() : Int = answers.map { numQuestionsEveryoneSaidYes(it) }.sum()
 
+    private fun numUniqueQuestionsAnsweredYes(groupAnswer: String) : Int =
+        groupAnswer.groupBy { it }
+            .count { it.key != '\n' }
+
+    private fun numQuestionsEveryoneSaidYes(groupAnswer : String) : Int {
+        val answerMap = groupAnswer.groupingBy { it }
+            .eachCount()
+        val numPeople = answerMap.getOrDefault('\n', 0) + 1
+
+        return answerMap.filterValues { it == numPeople }
+            .count()
+    }
 
     companion object {
         private fun parse(input : String) : List<String> = input.split("\n\n")
