d7bac4e3a327600f6e5dbbdfc23c39d2f83f1ac4
Luke Durkan <ldurkan@atlassian.com>
2020-12-12 03:31:21 +1100

    Simplified solution for Day10.

--- src/main/kotlin/advent2020/Day10.kt
+++ src/main/kotlin/advent2020/Day10.kt
@@ -4,32 +4,26 @@ package advent2020
 class Day10(rawInput : List<String>) {
     private val adapters = rawInput.map { it.toInt() }
         .sorted()
+        .let { it.plus(it.maxOrNull()!! + 3) }
 
-    fun solvePart1() : Int {
-        val joltCounts = mutableMapOf<Int, Int>()
-        //add device 3 jolt
-        joltCounts[3] = 1
-        var previousRating = 0
-
-        adapters
-            .forEach {
-                joltCounts.merge(it - previousRating, 1) {old, _ -> old.inc()}
-                previousRating = it
-            }
-
-        return joltCounts.getOrDefault(1, 0) * joltCounts.getOrDefault(3,0)
-    }
+    fun solvePart1() : Int =
+        (listOf(0) + adapters)
+            .zipWithNext()
+            .map { it.second - it.first }
+            .groupingBy { it }
+            .eachCount()
+            .let { it.getValue(1) * it.getValue(3) }
 
     fun solvePart2() : Long {
         val pathsToAdapter = mutableMapOf(0 to 1L)
 
-        adapters.plus(adapters.maxOrNull()!! + 3)
+        adapters
             .forEach {
                 pathsToAdapter[it] = (1..3).map { previous ->
                     pathsToAdapter.getOrDefault(it - previous, 0)
                 }.sum()
             }
 
-        return pathsToAdapter.getValue(adapters.maxOrNull()!! + 3)
+        return pathsToAdapter.getValue(adapters.last())
     }
 }
